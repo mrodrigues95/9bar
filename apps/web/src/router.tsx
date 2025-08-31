871316f1f869b6e5706724b3875724f9adb0e224
Marcus Rodrigues <marcus.rodrigues95@gmail.com>
2025-08-31 13:59:21 -0400

    feat: setup web app

---
+++ apps/web/src/router.tsx
@@ -0,0 +1,20 @@
+import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+export function createRouter() {
+  const router = createTanStackRouter({
+    routeTree,
+    defaultPreload: 'intent',
+    defaultErrorComponent: () => <div>Oh no! An error occurred!</div>,
+    defaultNotFoundComponent: () => <div>404! Page not found!</div>,
+    scrollRestoration: true,
+  })
+
+  return router
+}
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: ReturnType<typeof createRouter>
+  }
+}
