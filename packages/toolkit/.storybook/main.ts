457eb39eb4822401fd7e5ddbc1efb4905076b47c
Marcus Rodrigues <marcus.rodrigues95@gmail.com>
2025-09-01 11:30:24 -0400

    feat: setup storybook package

---
+++ packages/toolkit/.storybook/main.ts
@@ -0,0 +1,21 @@
+import { dirname, join } from "node:path";
+import type { StorybookConfig } from "@storybook/react-vite";
+
+function getAbsolutePath(value: string) {
+	return dirname(require.resolve(join(value, "package.json")));
+}
+
+const config: StorybookConfig = {
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-docs"),
+		getAbsolutePath("@storybook/addon-a11y"),
+	],
+	framework: {
+		name: getAbsolutePath("@storybook/react-vite"),
+		options: {},
+	},
+};
+
+export default config;
---
+++ packages/toolkit/.storybook/preview.ts
@@ -0,0 +1,16 @@
+import type { Preview } from "@storybook/react-vite";
+
+const preview: Preview = {
+	tags: ["autodocs"],
+	parameters: {
+		layout: "fullscreen",
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export default preview;
